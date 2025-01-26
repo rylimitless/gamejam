2b82b98df6a7d693ec33cfeccff354a16450a1ce
FelecityArt <kentonfelecia123@gmail.com>
2025-01-26 00:31:03 -0500

    Level-01

---
+++ pop/lib/pop.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+import 'package:pop/levels/level.dart';
+import 'package:flame/components.dart';
+
+class Pop extends FlameGame {
+  
+  @override 
+  Color backgroundColor() => const Color(0xFF211F30);
+
+  late final CameraComponent cam;
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+
+    add(Level());
+    return super.onLoad();
+  }
+}
